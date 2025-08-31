ccf0e1de5127081ed74f959e88fa49867d786af7
kaviyarasuvengidusamy-create <kaviyarasuvengidusamy@gmail.com>
2025-08-31 15:37:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio 
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Kavi-Kavi-the-typescripter/pen/VYvBJOz](https://codepen.io/Kavi-Kavi-the-typescripter/pen/VYvBJOz).
+
---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll("nav a").forEach(link => {
+
+  link.addEventListener("click", e => {
+
+    e.preventDefault();
+
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
+
+      behavior: "smooth"
+
+    });
+
+  });
+
+});
